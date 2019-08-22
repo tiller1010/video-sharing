6ed913f392d37da370166a43a1e3247a8513479f
Tyler <tiller1010@gmail.com>
2019-08-22 15:13:56 -0400

    Added video data objects and a simple page template

---
+++ app/src/videos/VideoObject.php
@@ -0,0 +1,38 @@
+<?php
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Assets\File;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\TextField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\FieldsList;
+
+class VideoObject extends DataObject {
+
+	private static $db = [
+		'VideoTitle' => 'Text',
+		'VideoDescription' => 'Text'
+	];
+
+	private static $has_one = [
+		'VideoSource' => File::class,
+		'VideoThumbnail' => Image::class,
+		'UploadedBy' => UserObject::class,
+		'VideoPage' => VideoPage::class
+	];
+
+	private static $summary_fields = [
+		'VideoTitle'
+	];
+
+	public function getCMSField()
+	{
+		$title = TextField::create('VideoTitle', 'Title');
+		$description = TextField::create('VideoDescription', 'Description');
+
+		return new FieldsList(
+			$title,
+			$description
+		);
+	}
+}
